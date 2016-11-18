938666506c4b2e6aaf9d50befbef451bbc36c713
magdalenek <ms915@ic.ac.uk>
2016-11-18 15:05:53 +0000

    Update CameraLauncher.java

--- LinkIt/app/src/main/java/le_project/com/linkit/CameraLauncher.java
+++ LinkIt/app/src/main/java/le_project/com/linkit/CameraLauncher.java
@@ -78,7 +78,6 @@ public class CameraLauncher extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.camera_layout, menu);
         return true;
     }
